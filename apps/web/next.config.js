e01ea6e9ecb3c06022407f47bb79c491a829068b
charlesauthur477-ui <charlesauthur477@gmail.com>
2026-08-06 01:08:11 +0530

    Update next.config.js

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -4,6 +4,8 @@ const path = require("path");
 const nextConfig = {
   reactStrictMode: true,
   output: "standalone",
-  outputFileTracingRoot: path.join(__dirname, "../../"),
+  experimental: {
+    outputFileTracingRoot: path.join(__dirname, "../../"),
+  },
 };
 module.exports = nextConfig;
